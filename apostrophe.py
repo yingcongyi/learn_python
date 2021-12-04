19a70719e8a415c1214a10fac2af302320ac2fad
zhangpeng <zhangpengyf@gmail.com>
2021-12-04 18:33:51 +0800

    learn python

---
+++ apostrophe.py
@@ -0,0 +1,12 @@
+message = "one of python's strengths is its diverse community."
+print(message)
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+
+print(message)
+
+n = 8
+message = "My favorite number is " + str(n) + "!"
+
+print(message)
